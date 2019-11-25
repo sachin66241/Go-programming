881a80b50a8c50899f91a715350960bb884cb64b
SACHIN <sachhin6624@gmail.com>
2019-11-25 09:41:13 +0530

    default value of variable is zero

--- hello.go
+++ hello.go
@@ -1,8 +1,10 @@
 package main
 
 func main() {
-	var num1 = 5
-	var num2 = 10
+	var num1 int // the default value of veraible will be zero
+	var num2 int
+	num1 = 10
+	num2 = 20
 	var result = num1 + num2
 	println(result)
 }
