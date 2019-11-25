0984227100048d3666a14a661f0c30f73822877f
SACHIN <sachhin6624@gmail.com>
2019-11-25 10:56:14 +0530

    addition and subtraction

--- add_sub/add_sub.go
+++ add_sub/add_sub.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+func add(x int, y int) (int, int) {
+	res1 := x + y
+	res2 := x - y
+	return res1, res2
+}
+
 func main() {
 	var num1 int // the default value of veraible will be zero
 	var num2 int
@@ -15,5 +21,8 @@ func main() {
 		fmt.Println(result, ":", i)
 
 	}
+
+	myres1, myres2 := add(num1, num2)
+	fmt.Println(myres1, myres2)
 
 }
